8bee110ddecf96fa62762c3816b0164b2ed18b2b
Thealexabang <alexaalvarez.97@gmail.com>
2023-08-13 19:46:28 -0300

    2ndattempt

--- eventos.js
+++ eventos.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const boton= document.getElementById('boton');
 
     boton.addEventListener("dblclick", function(){
+        Event.stopPropagation();
         alert("Hola! Soy el div");
     });
 
